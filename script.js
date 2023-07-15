a4ebd23ebda40e8ae3e39099acf743064ea6b489
amalali8 <amalza958@gmail.com>
2023-07-16 01:03:19 +0300

    answer

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello");
+function caltip(bill){
+    let tip;
+    return tip
+    }
+    let bill = 795
+    if (bill >= 50 && bill <= 300) {
+      tip = bill * 0.15; 
+    } else {
+      tip = bill * 0.2; 
+    } 
+    let totalValue = bill + tip;
+    console.log(
+      `The bill was ${bill}, the tip was ${tip}, and the total value is ${totalValue}`
+    );
+    
